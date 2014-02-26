c8637a6286fe563bc19d6426d722a1c05ca5c31b
ericmoura <ericmoura@hotmail.com>
2014-02-25 23:32:03 -0300

    Attt Traffic Class, Naive Bayes melhorado sem a discretização e Kmeans parametrizado.

--- src/main/TrafficJamDetector.java
+++ src/main/TrafficJamDetector.java
@@ -2,10 +2,13 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import tutorials.TutorialData;
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.bayes.NaiveBayesClassifier;
+import net.sf.javaml.classification.evaluation.EvaluateDataset;
+import net.sf.javaml.classification.evaluation.PerformanceMeasure;
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.clustering.KMeans;
 import net.sf.javaml.clustering.evaluation.AICScore;
@@ -21,8 +24,8 @@ public class TrafficJamDetector {
 
 
 	
-	private static void kmeans(String file) throws IOException{
-		Dataset data = FileHandler.loadDataset(new File(file), 18, "	");
+	private static void kmeans(String file, int classPosition) throws IOException{
+		Dataset data = FileHandler.loadDataset(new File(file), classPosition, "	");
 		
 		Clusterer km = new KMeans(3);
 		Dataset[] clusters = km.cluster(data);
@@ -43,30 +46,30 @@ public class TrafficJamDetector {
 	}
 	
 	
-	private static void naiveBayes() throws IOException{
+	private static void naiveBayes(String file, int classPosition) throws IOException{
 		/* Load a data set */
-		Dataset data = TutorialData.IRIS.load();
+		Dataset data = FileHandler.loadDataset(new File(file), classPosition, "	");
 
 		/* Discretize through EqualWidtBinning */
-		EqualWidthBinning eb = new EqualWidthBinning(20);
-		System.out.println("Start discretisation");
-		eb.build(data);
-		Dataset ddata = data.copy();
-		eb.filter(ddata);
+		//EqualWidthBinning eb = new EqualWidthBinning(100);
+		//System.out.println("Start discretisation");
+		//eb.build(data);
+		//Dataset ddata = data.copy();
+		//eb.filter(ddata);
 
 		boolean useLaplace = true;
 		boolean useLogs = true;
 		Classifier nbc = new NaiveBayesClassifier(useLaplace, useLogs, false);
 		nbc.buildClassifier(data);
 
-		Dataset dataForClassification = TutorialData.IRIS.load();
+		Dataset dataForClassification = FileHandler.loadDataset(new File(file), 18, "	");
 
 		/* Counters for correct and wrong predictions. */
 		int correct = 0, wrong = 0;
 
 		/* Classify all instances and check with the correct class values */
 		for (Instance inst : dataForClassification) {
-			eb.filter(inst);
+			//eb.filter(inst);
 			Object predictedClassValue = nbc.classify(inst);
 			Object realClassValue = inst.classValue();
 			if (predictedClassValue.equals(realClassValue))
@@ -78,11 +81,21 @@ public class TrafficJamDetector {
 		}
 		System.out.println("correct " + correct);
 		System.out.println("incorrect " + wrong);
+		
+		Map<Object, PerformanceMeasure> pm = EvaluateDataset.testDataset(nbc, dataForClassification);
+        for (Object o : pm.keySet()){
+        	System.out.println("------"+o+"------");
+        	System.out.println("Accuracy : " + pm.get(o).getAccuracy());
+        	System.out.println("Reccal : " + pm.get(o).getRecall());
+        	System.out.println("Precision : " + pm.get(o).getPrecision());
+        }
+            
 	}
 	
 	
 	public static void main(String[] args) throws IOException {
-		kmeans("pems_splitData_NB.csv");
+		naiveBayes("pems_splitData_NB.csv", 18);
+		kmeans("pems_splitData_NB.csv", 18);
 	}
 
 }
